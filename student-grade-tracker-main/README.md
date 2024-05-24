cfe0f11780e38f418fa692396b5a8d99caf3f0e9
Sujaykc <162112960+Sujaykc@users.noreply.github.com>
2024-05-24 22:31:22 +0530

    Add files via upload

---
+++ student-grade-tracker-main/README.md
@@ -0,0 +1,2 @@
+# student-grade-tracker
+Student grade tracker using java. Java program that allows to enter students' grades and computes their average, highest, and lowest scores using ArrayLists.
---
+++ student-grade-tracker-main/StudentGradeTracker.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class StudentGradeTracker {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> grades = new ArrayList<>();
+
+        System.out.print("Enter the number of students: ");
+        int numStudents = scanner.nextInt();
+
+        for(int i = 0; i < numStudents; i++){
+            System.out.print("Enter grade for student " + (i + 1) + ":");
+            int grade = scanner.nextInt();
+            grades.add(grade);
+        }
+
+        if (!grades.isEmpty()){
+            int sum = 0;
+            int highest = grades.get(0);
+            int lowest = grades.get(0);
+
+            for (int grade : grades){
+                sum += grade;
+                if (grade > highest){
+                    highest = grade;
+
+                }
+                if (grade < lowest){
+                    lowest = grade;
+                }
+                        
+        }
+
+        double average = (double) sum / numStudents;
+
+        System.out.println("Grades entered: " + grades);
+        System.out.println("Average grade: " + average);
+        System.out.println("Highest grade: " + highest);
+        System.out.println("Lowest grade: " + lowest);
+
+    }else{
+        System.out.println("No grades entered.");
+    }
+    scanner.close();
+}
+
+}
